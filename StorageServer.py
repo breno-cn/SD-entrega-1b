4bc18d23c71e8bee75aa32e6fdfcba4dcc8c1c58
breno-cn <breno.cn@hotmail.com>
2022-01-23 17:20:13 -0300

    rascunho inicial de servidor de armazenamento

--- StorageServer.py
+++ StorageServer.py
@@ -1,11 +1,15 @@
 from ClientToPage_pb2_grpc import PageStub
 from ClientToPage_pb2 import AnnounceRequest
 
+from Storage_pb2_grpc import StorageServicer
+from Storage_pb2 import StorageResponse
+
 from Hashtable import Hashtable
+from concurrent import futures
 import sys
 import grpc
 
-class StorageServer:
+class StorageServer(StorageServicer):
 
     def __init__(self, name: str, host: str, port: int) -> None:
         self.hashtable = Hashtable()
@@ -26,5 +30,53 @@ class StorageServer:
             
             print('Servidor de armazenamento criado com sucesso...')
 
+    def create(self, request, context):
+        key = request.key
+        value = request.value
+        print(f'key {key} value {value}')
 
-server = StorageServer('test', 'localhost', 12345)
+        status = self.hashtable.create(key, value)
+
+        return StorageResponse(status=status)
+    
+    def read(self, request, context):
+        key = request.key
+        readResponse = self.hashtable.read(key)
+        
+        print(readResponse)
+
+        if type(readResponse) == str:
+            return StorageResponse(status=4, value=readResponse)
+
+        return StorageResponse(status=5, value='')
+
+    def update(self, request, context):
+        key = request.key
+        value = request.value
+        status = self.hashtable.update(key, value)
+
+        return StorageResponse(status=status)
+
+    def delete(self, request, context):
+        key = request.key
+        status = self.hashtable.delete(key)
+
+        return StorageResponse(status=status)
+
+
+
+def serve():
+    name = 'test'
+    host = 'localhost'
+    port = 12345
+    storageServer = StorageServer(name, host, port)
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    # add_PageServicer_to_server(PageServer(), server)
+
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+
+    server.wait_for_termination()
+
+serve()
